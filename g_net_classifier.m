1569f3ccd2e8767051ebec016ec5fd67ec8c9a12
cmiddlemiss <cmiddlemiss@brandeis.edu>
2018-05-04 17:03:02 -0400

    added file for .tex final paper

--- g_net_classifier.m
+++ g_net_classifier.m
@@ -1,7 +1,8 @@
-function [label] = untitled(I)
+function [label] = g_net_classifier(I)
 %uses function of googlenet to classify and image 
 %  label = ballplayer/person means player
 I = imresize(I,[224 224]);
+imshow(I)
 net = googlenet;
 inputSize = net.Layers(1).InputSize;
 
